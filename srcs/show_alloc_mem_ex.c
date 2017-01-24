4d22c4cf17b181efcfb6ff1f239bbad5388073e2
ulefebvr <ulefebvr@student.42.fr>
2017-01-24 02:12:45 +0100

    [TOOL] printf to ft_printf

--- srcs/show_alloc_mem_ex.c
+++ srcs/show_alloc_mem_ex.c
@@ -6,7 +6,8 @@ static void	show_all_blocks(t_lst *block)
 	{
 		if (!(BLOCK(block)->is_free))
 		{
-			ft_printf("%p - %p : %ld octets\n",
+			ft_printf("%k %p - %p : %ld octets\n",
+				&BLOCK(block)->time,
 				(void *)BLOCK(block) + sizeof(t_header_block),
 				(void *)BLOCK(block) + BLOCK(block)->size
 				+ sizeof(t_header_block), BLOCK(block)->size
@@ -20,13 +21,14 @@ static void	show_all_pages(t_lst *pages, char *type)
 {
 	while (pages)
 	{
-		ft_printf("%s : %p\n", type, (void *)PAGE(pages) + sizeof(t_header_page));
+		ft_printf("%s : %p\n", type, 
+			(void *)PAGE(pages) + sizeof(t_header_page));
 		show_all_blocks(PAGE(pages)->block);
 		pages = pages->next;
 	}
 }
 
-void		show_alloc_mem(void)
+void		show_alloc_mem_ex(void)
 {
 	show_all_pages(g_malloc.tiny_list, "TINY");
 	show_all_pages(g_malloc.small_list, "SMALL");
