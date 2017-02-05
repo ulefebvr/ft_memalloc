cdb98769ba559df85affaff811c9318f503bf06e
ulefebvr <ulefebvr@student.42.fr>
2017-02-05 21:35:34 +0100

    [THREAD] Mutex init

--- srcs/private/malloc_initialize.c
+++ srcs/private/malloc_initialize.c
@@ -12,9 +12,12 @@
 
 #include "private_malloc.h"
 
+#include <pthread.h>
+
 int	malloc_initialize(void)
 {
 	ft_bzero(&g_malloc, sizeof(t_malloc));
 	g_malloc.init = 1;
+	pthread_mutex_init(&g_malloc_lock, 0);
 	return (g_malloc.init);
 }
