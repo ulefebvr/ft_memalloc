5c2c07c8e64e4c032c4eb39943c2b470a70e265f
ulefebvr <ulefebvr@student.42.fr>
2017-02-11 03:25:23 +0100

    [SCRIPT] Dynamic library overload script fix

--- test/run.sh
+++ test/run.sh
@@ -2,6 +2,7 @@
 
 if [ "$(uname)" == "Linux" ]; then
 	export LD_LIBRARY_PATH=$(dirname $(readlink -f ../libft_malloc.so))/
+	export LD_PRELOAD=$(readlink -f ../libft_malloc.so)
 else
 	export DYLD_LIBRARY_PATH=../
 	export DYLD_INSERT_LIBRARIES="../libft_malloc.so"
